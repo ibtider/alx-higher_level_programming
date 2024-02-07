a92a27d4e05324c55ced1830a2807629f9cce6e5
ibtider <btissamderouich33@.com>
2024-02-07 15:30:01 -0800

    new update

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
-
+#include <stdio.h>
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
 void print_python_float(PyObject *p);
